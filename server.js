76e13708cd429cc98df510061d2f7942eb357c59
AjaniBrowne <ajani.browne@yahoo.com>
2022-07-27 17:57:21 -0400

    commit on main"started server.js"

---
+++ server.js
@@ -0,0 +1,21 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+
+
+
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'rootroot',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+  
